d2004cfeaaee79a6a7785282aee51db64633379e
Owen Hillary <hillaryo@mail.gvsu.edu>
2026-02-23 10:23:39 -0500

    Commented main.cpp and Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -3,34 +3,49 @@
 #include "map.h"
 #include "ECS/Component.h"
 
-
+// Global variablesq
 Map* map;
 
 SDL_Renderer* Game::renderer = nullptr;
 Manager manager;
 auto& Player(manager.addEntity());
 
+// Constructor and Destructor  < -------- Not sure if these are necessary, but they are here for now.
 Game::Game() {
-
 }
 Game::~Game() {
-
 }
 
+// Creates instance of game
 void Game::init(const char *title,int width, int height, bool fullscreen) {
+
+    // Sets up flags for SDL (Just fullscreen for now, but can be expanded later if needed)
     int flags = 0;
     if (fullscreen) {
         flags = SDL_WINDOW_FULLSCREEN;
     }
+
+    // Initializes SDL, error checks, and creates window and renderer. Also initializes map and player entity with position and sprite components.
     if (!SDL_Init(SDL_INIT_VIDEO)) {
+        // Error Checking
         SDL_Log("SDL_Init failed: %s", SDL_GetError());
     }
     else {
+        // Initialization successful, create window and renderer
+
         std::cout << "game running" << std::endl;
+
+        // Creates window
         window = SDL_CreateWindow(title,width,height,flags);
         if (window) {
             std::cout << "Window Created" << std::endl;
+        } else {
+            // Error
+            SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
+            isRunning = false;
         }
+
+        // Creates renderer
         renderer = SDL_CreateRenderer(window,NULL);
         if (renderer) {
             std::cout << "Render Created" << std::endl;
@@ -38,21 +53,27 @@ void Game::init(const char *title,int width, int height, bool fullscreen) {
             isRunning = true;
         }
         else {
+            // Error
             SDL_Log("SDL_render failed: %s", SDL_GetError());
             isRunning = false;
         }
+
+        // Initialize map and player entity with position and sprite components
         map = new Map();
         Player.addComponent<PositionComponent>(0,450);
         Player.addComponent<SpriteComponent>("textures/Reaper.png");
     }
-
-
-
 }
 
+
+// Handles events, currently just checks for quit event
 void Game::handleEvents() {
     SDL_Event event;
+
+    // See if there is an event to process
     while (SDL_PollEvent(&event)) {
+
+        // Check event type and handle accordingly
         switch (event.type) {
             case SDL_EVENT_QUIT:
                 isRunning = false;
@@ -65,21 +86,24 @@ void Game::handleEvents() {
 
 }
 
+// Updates game state
 void Game::update() {
     manager.refresh();
     manager.update();
-
-
 }
 
+// Renders to screen
 void Game::render() {
+    // Clear previous render
     SDL_RenderClear(renderer);
+    
     // Add items to render
     map -> DrawMap();
     manager.draw();
     SDL_RenderPresent(renderer);
 }
 
+// Cleans up memory and quits SDL
 void Game::clean() {
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
--- main.cpp
+++ main.cpp
@@ -3,23 +3,35 @@
 Game *game = nullptr;
 int main(int argc,const char * argv[]) {
 
+
+    // Set up frame rate
     const int FPS = 60;
     const int frameDelay = 1000/FPS;
 
     Uint32 frameStart;
     int frameTime;
 
+    // Creates Instance of Game and initializes it
     game = new Game;
     game -> init("test",800,640,false);
 
+    // Begins game loop
     while (game -> running()) {
 
+        // Calculates frame time and delays if necessary to maintain frame rate
         frameStart = SDL_GetTicks();
+
+        // Processes event list
         game -> handleEvents();
+
+        // Updates game state and renders to screen
         game -> update();
         game -> render();
+
+        // Calculates time taken for frame
         frameTime = SDL_GetTicks() - frameStart;
 
+        // Delays if frame time is less than desired frame delay to maintain consistent frame rate
         if (frameDelay > frameTime) {
             SDL_Delay(frameDelay -frameTime);
         }
