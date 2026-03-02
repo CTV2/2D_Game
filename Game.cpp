3d20b2d355ac083490c2070cd70e401788bb51fe
Owen Hillary <hillaryo@mail.gvsu.edu>
2026-03-02 11:30:28 -0500

    Didnt change anything

--- Game.cpp
+++ Game.cpp
@@ -132,10 +132,6 @@ void Game::handleEvents() {
                         Player->addComponent<ColliderComponent>(16, 16);
                         Player->addComponent<SpriteComponent>("textures/Reaper.png");
                         Game::randomSpawn();
-                        // Tree_0->addComponent<PositionComponent>(0,430);
-                        // Tree_0->addComponent<ColliderComponent>(120, 120, 15, 10);
-                        // Tree_0->addComponent<SpriteComponent>("textures/up_tree.png",0.50);
-                       
                     }
                     if (event.key.key == SDLK_ESCAPE) {
                         isRunning = false;
